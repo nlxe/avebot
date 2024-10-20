5fe6622e6292b878196c9eb3b6a151ffa3eaf361
Mr.Neluxe <iyedrjab84@gmail.com>
2024-10-20 16:10:26 +0000

    Initial commit

---
+++ Settings/Categories.js
@@ -0,0 +1,37 @@
+/**
+ * @type {import("Types.d.ts").CommandsCategories[]}
+ */
+const categories = [
+  {
+    category: "ADMIN",
+    description: "Commands for server administration and management.",
+    emoji: "⚙",
+    image: "https://cdn.discordapp.com/emojis/1112381870089449572.png"
+  },
+  {
+    category: "MODERATION",
+    description: "Commands to assist with moderating and controlling your server.",
+    emoji: "🛡",
+    image: "https://twemoji.maxcdn.com/v/latest/72x72/1f528.png"
+  },
+  {
+    category: "INFORMATION",
+    description: "Commands that provide useful information and data.",
+    emoji: "📄",
+    image: "https://twemoji.maxcdn.com/v/latest/72x72/1f5de.png"
+  },
+  {
+    category: "UTILITY",
+    description: "Utility commands that offer various helpful functionalities.",
+    emoji: "🔧",
+    image: "https://twemoji.maxcdn.com/v/latest/72x72/2692.png"
+  },
+  {
+    category: "MUSIC",
+    description: "Commands for playing and managing music.",
+    emoji: "🎶",
+    image: "https://twemoji.maxcdn.com/v/latest/72x72/1f3b5.png"
+  }
+];
+
+export default categories;
